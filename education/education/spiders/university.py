60d3f29baf193fa4ead65595e6add02d36f57787
niyoufa <niyoufa@tmlsystem.com>
2016-10-09 00:50:35 +0800

    education

--- education/education/spiders/university.py
+++ education/education/spiders/university.py
@@ -39,5 +39,89 @@ class UniversitySpider(Spider):
                 items.append(item)
         return items
 
+#抓取中国高校院校等级相关信息
+class University1Spider(Spider):
+    name = "university1"
+    allowed_domains = ["chinadegrees.cn"]
+    start_urls  = ["http://www.chinadegrees.cn/xwyyjsjyxx/xwsytjxx/274346.shtml",
+                            "http://www.chinadegrees.cn/xwyyjsjyxx/xwsytjxx/274346_2.shtml",
+                            "http://www.chinadegrees.cn/xwyyjsjyxx/xwsytjxx/274346_3.shtml"]
+
+    def parse(self, response):
+        sel = Selector(response)
+        tbody = sel.xpath("//table[1]/tbody")
+        trs = tbody.xpath("tr")
+
+        items = []
+        for tr in trs[1:]:
+            tds = tr.xpath("td")
+            try:
+                name = tds[0].xpath("p/span/text()").extract()[0]
+            except:
+                continue
+            query_params = {"name":name}
+            university = university_coll.find_one(query_params)
+            if university :
+                university["type"] = tds[1].xpath("p/span/text()").extract()[0]
+                if tds[4].xpath("p/span/text()") == []:
+                    university["is_211"] = False
+                else:
+                    university["is_211"] = True
+
+                if tds[5].xpath("p/span/text()") == []:
+                    university["is_985"] = False
+                else:
+                    university["is_985"] = True
+
+                if tds[6].xpath("p/span/text()") == []:
+                    university["has_graduate_school"] = False
+                else:
+                    university["has_graduate_school"] = True
+                university_coll.save(university)
+        return items
+
+#抓取中国高校本科招生网站链接
+i = 0
+class University2Spider(Spider):
+    name = "university2"
+    allowed_domains = ["gk114.com"]
+    start_urls  = ["http://www.gk114.com/"]
+
+    def parse(self, response):
+        sel = Selector(response)
+        trs = sel.xpath("/html/body/table[12]/tr/td[1]/table/tr/td/table/tr[2]/td/table/tr")
+
+        new_urls = []
+        for tr in trs:
+            tds = tr.xpath("td")
+            for td in tds:
+                new_url = "http://www.gk114.com/" + td.xpath("a/@href").extract()[0]
+                new_urls.append(new_url)
+
+        print new_urls
+        for new_url in new_urls:
+            yield Request(url=new_url, callback=self.parse_post)
+
+    def parse_post(self, response):
+        sel = Selector(response)
+        lis = sel.xpath("//ul/li")
+        items = []
+        for li in lis:
+            try:
+                name = li.xpath("a/font/text()").extract()[0]
+            except:
+                name = li.xpath("a/text()").extract()[0]
+
+            undergraduate_url = li.xpath("a/@href").extract()[0]
+            query_params = {
+                "name":name,
+            }
+            university = university_coll.find_one(query_params)
+            if university:
+                university["undergraduate_url"] = undergraduate_url
+                university_coll.save(university)
+
+            print name,undergraduate_url
+
 
 
